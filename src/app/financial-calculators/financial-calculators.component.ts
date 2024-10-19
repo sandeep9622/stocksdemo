b71985020464b8c245c7fcebe014726cc55349b3
sandeep singh <sandeep.sappal96@gmail.com>
2024-10-19 23:53:13 +0530

    some code changes

--- src/app/financial-calculators/financial-calculators.component.ts
+++ src/app/financial-calculators/financial-calculators.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { UtilitiesService } from '../services/utilities.service';
 
 @Component({
   selector: 'app-financial-calculators',
@@ -7,15 +8,18 @@ import { FormBuilder, FormGroup, Validators } from '@angular/forms';
   styleUrls: ['./financial-calculators.component.css']
 })
 export class FinancialCalculatorsComponent {
-  lumpsumForm!:FormGroup;
+  lumpsumForm!: FormGroup;
 
-  constructor(private fb:FormBuilder){}
+  onlyNumeric = this.service.integerValidation;
+  numericDecimal = this.service.decimalValidation;
 
-  ngOnInit(){
+  constructor(private fb: FormBuilder, private service: UtilitiesService) { }
+
+  ngOnInit() {
     this.lumpsumForm = this.fb.group({
-      principal:["",[Validators.required]],
-      rate:['',[Validators.required]],
-      time:['',[Validators.required]]
+      principal: ["", [Validators.required]],
+      rate: ['', [Validators.required]],
+      time: ['', [Validators.required]]
     })
   }
 }
