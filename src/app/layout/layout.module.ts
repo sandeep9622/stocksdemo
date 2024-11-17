133792da4fc84cb159e0ec3b67ddbc3dee60b9c2
Sandeep singh <sandeep.sappal96@gmail.com>
2024-11-17 20:00:05 +0530

    added social media icons, published the app on github pages, added nav bar functionality for mobile devices.

--- src/app/layout/layout.module.ts
+++ src/app/layout/layout.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { LayoutComponent } from './layout.component';
 import { RouterModule } from '@angular/router';
+import { MatIconModule } from '@angular/material/icon';
 
 
 
@@ -11,7 +12,8 @@ import { RouterModule } from '@angular/router';
   ],
   imports: [
     CommonModule,
-    RouterModule
+    RouterModule,
+    MatIconModule
   ],
   exports:[
     LayoutComponent
