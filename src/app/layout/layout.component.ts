9fe6c063f8058e1311f977318f18b6f3f1e8eb3f
Sandeep singh <sandeep.sappal96@gmail.com>
2025-05-17 18:15:18 +0530

    Refactor navbar to use dynamic links and improve URL tracking functionality

--- src/app/layout/layout.component.ts
+++ src/app/layout/layout.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import {Router,NavigationEnd} from '@angular/router'
+import { Router, NavigationEnd } from '@angular/router';
 import { filter } from 'rxjs';
 
 @Component({
@@ -8,14 +8,37 @@ import { filter } from 'rxjs';
   styleUrls: ['./layout.component.css']
 })
 export class LayoutComponent {
-  urlText:string = 'home';
-  public constructor(router:Router){
-    router.events.pipe(filter( event => event instanceof NavigationEnd))
-    .subscribe((res) => {
-      if(res instanceof NavigationEnd){
-        console.log(res);
-        this.urlText = res.url;
-      }
-    })
+  urlText: string = ''; // Current URL
+
+  // Define navigation links dynamically
+  navLinks = [
+    { label: 'Home', path: '/home' },
+    { label: 'Portfolio Tracker', path: '/portfoliotracker' },
+    { label: 'Investments Timeline Tracker', path: '/investmentstimeline' },
+    { label: 'Allocation Splitter', path: '/allocationsplitter' },
+    { label: 'Financial Calculators', path: '/financialcalculators' }
+  ];
+
+  externalLinks = [
+    {
+      label: 'GitHub',
+      url: 'https://github.com/sandeep9622/stocksdemo',
+      icon: 'github'
+    },
+    {
+      label: 'LinkedIn',
+      url: 'https://www.linkedin.com/in/sandeep-singh-8819841a1/',
+      icon: 'linkedin'
+    }
+  ];
+
+  constructor(router: Router) {
+    // Listen to navigation events to update the active URL
+    router.events.pipe(filter(event => event instanceof NavigationEnd))
+      .subscribe((res) => {
+        if (res instanceof NavigationEnd) {
+          this.urlText = res.url; // Update the current URL
+        }
+      });
   }
 }
